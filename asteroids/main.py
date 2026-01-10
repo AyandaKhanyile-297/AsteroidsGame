cd0350ab67e08f65186df3839cd36ac97796e62c
Mr Khanyile <Khanyile_Ayanda@proton.me>
2026-01-10 23:34:05 +0200

    H: added game loop to main

--- asteroids/main.py
+++ asteroids/main.py
@@ -9,5 +9,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    while True:
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
+    
 if __name__ == "__main__":
     main()
