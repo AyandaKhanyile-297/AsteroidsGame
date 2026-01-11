d60a2c95401ac502129d7a5b28c25dce556ab119
AyandaKhanyile-297 <Khanyile_Ayanda@proton.me>
2026-01-11 18:29:06 +0200

    L: added player.py and modified constants and main

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,15 +1,20 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+import player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    playerX = Player(x, y) 
     
     while True:
         log_state()
@@ -19,6 +24,7 @@ def main():
                 return
         
         screen.fill("black")
+        playerX.draw(screen)
         pygame.display.flip()
         
         dt = (clock.tick(60))/1000
