dc167cfba7aa1523165d9c5d0392caedebbb54af
Mr Khanyile <Khanyile_Ayanda@proton.me>
2026-01-12 20:27:06 +0200

    Add move method to player for movement control

--- asteroids/player.py
+++ asteroids/player.py
@@ -30,3 +30,9 @@ class Player(CircleShape):
     if keys[pygame.K_d]:
       # rotate right
       rotate(dt)
+
+	def move(self, dt):
+    unit_vector = pygame.Vector2(0, 1)
+		rotated_vector = unit_vector.rotate(self.rotation)
+		rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+		self.position += rotated_with_speed_vector
