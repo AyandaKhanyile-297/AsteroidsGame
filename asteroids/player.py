53abc1949521f897e40aecbd5b38615721e07871
AyandaKhanyile-297 <Khanyile_Ayanda@proton.me>
2026-01-16 00:17:52 +0200

    R: fixed indentations on player

--- asteroids/player.py
+++ asteroids/player.py
@@ -37,8 +37,8 @@ class Player(CircleShape):
       # rotate forwards
       rotate(dt)
 
-	def move(self, dt):
+  def move(self, dt):
     unit_vector = pygame.Vector2(0, 1)
-		rotated_vector = unit_vector.rotate(self.rotation)
-		rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-		self.position += rotated_with_speed_vector
+    rotated_vector = unit_vector.rotate(self.rotation)
+    rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+    self.position += rotated_with_speed_vector
