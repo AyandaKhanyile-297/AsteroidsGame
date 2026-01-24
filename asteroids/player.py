f4e4baf438085e5cdd77c54dfc2d15d2dfd29ecb
AyandaKhanyile-297 <Khanyile_Ayanda@proton.me>
2026-01-24 19:33:13 +0200

    T: updated movement for player.py

--- asteroids/player.py
+++ asteroids/player.py
@@ -26,10 +26,10 @@ class Player(CircleShape):
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
       # rotate left
-      move(-1*dt)
+      self.move(-1*dt)
     if keys[pygame.K_d]:
       # rotate right
-      move(dt)
+      self.move(dt)
     if keys[pygame.K_s]:
       # rotate backwards
       rotate(-1*dt)
